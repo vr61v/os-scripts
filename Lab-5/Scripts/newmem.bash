6797b10df38ca6f08d325d3f5d2967cfaeb99979
Vr61v <nikita20040503@gmail.com>
2024-04-28 17:20:07 +0300

    feat: done 5th lab

---
+++ Lab-5/Scripts/newmem.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+array=()
+while true; do
+  array+=(1 2 3 4 5 6 7 8 9 10)
+  if [[ ${#array[@]} -gt $1 ]]; then exit 1; fi
+done
---
+++ Lab-5/Scripts/runnewmem.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+for (( i=0; i < $2; i++ )); do
+  ./newmem.bash "$1"&
+  sleep 1s
+done
